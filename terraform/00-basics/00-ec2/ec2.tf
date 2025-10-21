6bff202fc5fcac773762e2a6b2f9007658f0040a
manojkumar-tech267 <manojkumarparuchuri8@gmail.com>
2025-10-21 10:14:24 +0530

    cleared all errors

--- terraform/00-basics/00-ec2/ec2.tf
+++ terraform/00-basics/00-ec2/ec2.tf
@@ -8,28 +8,10 @@ resource "aws_instance" "example" {
   }
 }
 
-
-
 resource "aws_security_group" "roboshop" {
   name        = "allow_tls"
-  description = "This security group allows all traffic from internet"
+  description = "Allow TLS inbound traffic and all outbound traffic"
   #vpc_id      = aws_vpc.main.id
-
-   ingress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
 
   tags = {
     Name = "allow_tls"
