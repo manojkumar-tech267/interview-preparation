7db663f6994c71b0d57eb40593c03baf816a6bf8
manojkumar-tech267 <manojkumarparuchuri8@gmail.com>
2025-11-04 23:13:31 +0530

    Developed VPC module

---
+++ terraform/01-modules/modules-testing/vpc-test/vpc.tf
@@ -0,0 +1,9 @@
+module "aws_vpc" {
+    source = "../../modules/vpc"
+    project = "roboshop"
+    environment = "dev"
+    public_subnet_cidrs = ["10.0.1.0/24","10.0.2.0/24"]
+    private_subnet_cidrs = ["10.0.11.0/24","10.0.12.0/24"]
+    database_subnet_cidrs = ["10.0.21.0/24","10.0.22.0/24"]
+    is_peering_required = true
+}
